cf27f51db8395410098194c583388faae2605cfb
carlosdircio <carlangasdircionava@gmail.com>
2020-06-27 14:42:43 -0500

    Factorial con iteraciones

---
+++ Factorial/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Factorial.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Factorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Factorial(long numero) {
+            Factor factorial = new Factor(numero);
+            
+            return View(factorial);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Factorial/Models/Factor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Factorial.Models
+{
+    public class Factor
+    {
+
+        public Factor(long numFactorial) {
+            this.NumeroFactorial = numFactorial;
+            Iteraciones = new List<string>();
+            calcularFactorial(NumeroFactorial);
+        }
+
+        public long calcularFactorial(long num) {
+            NumResultado= 1;
+            
+            for (int i = 1; i <= num; i++)
+            {
+                String iteracion = NumResultado + " * " + i+" = ";
+                NumResultado = NumResultado * i;
+                iteracion  += NumResultado;
+                Iteraciones.Add(iteracion);
+
+            }
+
+
+
+            return NumResultado;
+        }
+
+        
+
+
+        public long NumeroFactorial { get; set; }
+        public long NumResultado { get; set; }
+        public List<string> Iteraciones { get; set; }
+
+    }
+}
